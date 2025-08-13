1b6d7651112ef1e14eedb863bc3a3c4e064e2778
Andy Hansen <github@andyhansen.co.nz>
2025-08-13 16:19:58 +1200

    ref(todo): adjust to use DTO

---
+++ DTOs/TodoItemDTO.cs
@@ -0,0 +1,44 @@
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+using TodoApi.Models;
+
+namespace TodoApi.DTOs;
+
+public class TodoItemDTO
+{
+  public long Id { get; set; }
+  public string? Name { get; set; }
+  public bool IsComplete { get; set; }
+  public int Order { get; set; } = 1;
+
+  public bool Update(TodoItem todoItem)
+  {
+    if (todoItem == null) return false;
+
+    todoItem.Name = Name;
+    todoItem.IsComplete = IsComplete;
+    todoItem.Order = Order;
+
+    return true;
+  }
+
+  public TodoItem Create()
+  {
+    return new TodoItem
+    {
+      Name = Name,
+      IsComplete = IsComplete,
+      Order = Order
+    };
+  }
+
+  public static TodoItemDTO Build(TodoItem todoItem)
+  {
+    return new TodoItemDTO
+    {
+      Id = todoItem.Id,
+      Name = todoItem.Name,
+      IsComplete = todoItem.IsComplete,
+      Order = todoItem.Order
+    };
+  }
+}
